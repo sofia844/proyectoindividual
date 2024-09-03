5488252535c1a0c2c3a56b7cc09c95c0478bbf61
sofia844 <sofiabotero2000@gmail.com>
2024-09-02 20:33:32 -0500

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,152 @@
+<?php
+include("bin/header.php");
+?>
+ <div class="row">
+          <div class="col-12" id="columna1">
+            <div class="movimiento">
+              <marquee behavior="alternate" direction="left" scrollamount="10">
+                <br>
+                ¡Bienvenido a nuestro rincón de belleza y estilo! 💄✨ Explora el fascinante mundo del maquillaje con MK SOFÍA. 💅 Descubre secretos, trucos y tutoriales para resaltar tu belleza única. 💖💋
+              </marquee>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12 col-md-4">
+
+            <div class="image">
+              <img src="IMAGENESPRINCIPAL/6.png" alt="Imagen 1" style="margin: 5px; width: 390px; height: 400px;">
+            </div>
+
+          </div>
+
+          <div class="col-xs-12 col-md-4">
+
+            <div class="image">
+              <img src="IMAGENESPRINCIPAL/2.png" alt="Imagen 1" style="margin: 5px; width: 390px; height: 400px;">
+            </div>
+            
+          </div>
+
+          <div class="col-xs-12 col-md-4">
+
+            <div class="image">
+              <img src="IMAGENESPRINCIPAL/7.png" alt="Imagen 1" style="margin: 5px; width: 390px; height: 400px;">
+            </div>
+            
+          </div>
+          
+        </div>
+
+        <br>
+        
+         
+
+        <section id="Información">
+          <div class="row">
+            <div class="col-12">
+              <br>
+              <br>
+              
+
+              <br>
+              <br>
+              <br>
+              <br>
+            <header>
+                <h1>¡Bienvenido a nuestro rincón de belleza y estilo! 💄✨</h1>
+            </header>
+        
+            <section>
+                <p>En nuestro sitio web, explorarás el fascinante mundo del maquillaje a través de información detallada y tutoriales prácticos.
+                    <br> 💅 Nos dedicamos a proporcionarte los secretos mejor guardados de la industria del maquillaje de una manera accesible y fácil de seguir. 🌟
+                <br>
+                Ya seas un principiante curioso o un amante experimentado del maquillaje, aquí encontrarás consejos, trucos y tutoriales cortos que te ayudarán
+                <br> a resaltar tu belleza única. 💖💋</p>
+                ¡Descubre y perfecciona tus habilidades de maquillaje con <strong>MK SOFÍA</strong>! ✨🌈</p>
+                <br>
+                <img src="IMAGENESLOGOS/3.png" style="border-radius: 50%; box-shadow: 0px 0px 10px 10px #ffbce5; width: 300px;">
+            </section>
+
+            <br>
+            <br>
+            <br>
+            <br>
+            
+            <section>
+              <p>Para obtener más detalles sobre nuestro emocionante proyecto, visita nuestro <a href="https://www.instagram.com/mkupsofiaf/" target="_blank"> <img src="IMAGENESPRINCIPAL/ig.png" alt="Instagram" class="rotar" style="width: 40px;"> </a>.</p>
+            </section>
+            
+            <br>
+            <br>
+            <br>
+            </div> 
+            </div> 
+        </section>
+
+        <div class="row">
+          <div class="col-12">
+            
+
+          <style>
+            @keyframes moverTexto {
+                0% {
+                    transform: translateX(0);
+                }
+                25% {
+                    transform: translateX(10px);
+                }
+                50% {
+                    transform: translateX(-10px);
+                }
+                75% {
+                    transform: translateX(10px);
+                }
+                100% {
+                    transform: translateX(0);
+                }
+            }
+    
+            h2 {
+                animation: moverTexto 1s infinite;
+            }
+
+        </style>
+        
+
+        
+        <section id="informacion-adicional">
+              <h2>Más sobre MK SOFÍA</h2>
+              <br>
+              <p>En MK SOFÍA, nos apasiona no solo resaltar tu belleza exterior, sino también empoderarte con conocimientos y técnicas que te permitan expresar tu creatividad a través del maquillaje. Explora nuestras secciones especiales:</p>
+              <ul>
+                  <li><strong>Blog de Belleza:</strong> Artículos informativos sobre las últimas tendencias y productos.</li>
+                  <br>
+                  <li><strong>Tutoriales Exclusivos:</strong> Accede a tutoriales detallados y exclusivos para crear looks impresionantes.</li>
+                  <br>
+                  <li><strong>Comunidad MK SOFÍA:</strong> Únete a nuestra comunidad, comparte tus experiencias y aprende de otros amantes del maquillaje.</li>
+              </ul>
+              <p>¡Sumérgete en nuestro universo de belleza y descubre todo lo que MK SOFÍA tiene para ofrecer!</p>
+        </section>
+        </div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+          <div class="col-12">
+            <img src="IMAGENESPRINCIPAL/8.png" style="border-radius: 50%; width: 300px;">
+          </div>
+        </div>
+    
+          
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<?php
+include("bin/footer.php");
+?>
